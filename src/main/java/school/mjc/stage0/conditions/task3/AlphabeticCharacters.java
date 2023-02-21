841de9066048599595488cf16ae969d07b7284e6
PavelDenisov <pavel.den111@gmail.com>
2023-02-21 21:32:49 +0200

    update V2

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if ((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z')) {
+            // check if character is a vowel
+            if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' ||
+                    character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U') {
+                System.out.println("Vowel");
+            } else {
+                System.out.println("Consonant");
+            }
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,25 +2,38 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-        case 8:
-        case 10:
-        case 12:
-        System.out.println("31 days");
-        break;
-        case 4:
-        case 6:
-        case 9:
-        case 11:
-        System.out.println("30 days");
-        break;
-        case 2:
-        System.out.println("28 or 29 days");
-        break;
-        default:
-        System.out.println("wrong number!");
+
+        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+            System.out.println("31 days");
+        }
+        else if (month == 4 || month == 6 || month == 9 || month == 11) {
+            System.out.println("30 days");
+        }
+        else if (month == 2) {
+            System.out.println("28 or 29 days");
+        }
+        else {
+            System.out.println("wrong number!");
+        }
+//        case 1:
+//        case 3:
+//        case 5:
+//        case 7:
+//        case 8:
+//        case 10:
+//        case 12:
+//        System.out.println("31 days");
+//        break;
+//        case 4:
+//        case 6:
+//        case 9:
+//        case 11:
+//        System.out.println("30 days");
+//        break;
+//        case 2:
+//        System.out.println("28 or 29 days");
+//        break;
+//        default:
+//        System.out.println("wrong number!");
     }
 }
